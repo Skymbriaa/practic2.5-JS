f43518ae2b94515c918f669331ca2d01082ff734
vova vasylenko <pozilaaskymbria@gmail.com>
2024-04-29 10:49:32 +0300

    Add 'filter'

---
+++ index.js
@@ -0,0 +1,10 @@
+// const arrNums1 = [1,5,9,4,6];
+
+// console.log(arrNums1.includes(56)); //key===elem
+// console.log(arrNums1.includes(5));
+
+//some
+
+const isFemale = user.filtre(function(user){
+    return user.isMale === false;
+})
