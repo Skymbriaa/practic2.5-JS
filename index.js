6520fec02d5705b97e764379867a2ce8a6e9154d
vova vasylenko <pozilaaskymbria@gmail.com>
2024-04-30 11:04:42 +0300

    Add example of previous

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 
 const sum1 = function (number1=2, number2=3) {
+  if(typeof number1 !=='number' || typeof number2 !== 'number'){
+    return NaN;
+  }
   return number1 + number2;
 };
 
 
-const sum2 = (number1=2, number2=3) => number1 + number2
+const sum2 = (number1=2, number2=3) => {
+  if(typeof number1 !=='number' || typeof number2 !== 'number'){
+    return NaN;
+  }number1 + number2
+};
