cfe0419c9ad8aee3d83449d5fabe5f5d0a60c974
vova vasylenko <pozilaaskymbria@gmail.com>
2024-04-30 11:48:50 +0300

    Add ...

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 
-const sum1 = function (number1=2, number2=3) {
-  if(typeof number1 !=='number' || typeof number2 !== 'number'){
-    return NaN;
-  }
-  return number1 + number2;
-};
+const sumNumbers1 = function() {
+ return Array.from(arguments).reduce((summa, elem)=> summa+elem)
+}
 
+const result = sumNumbers1(1,2,3);
 
-const sum2 = (number1=2, number2=3) => {
-  if(typeof number1 !=='number' || typeof number2 !== 'number'){
-    return NaN;
-  }number1 + number2
-};
+console.log(result);
+//
+//
+//
+const sumNumbers2 = (...rest) => rest.reduce((summa, elem)=>summa+elem);
+
+// const sumNumbers2 = () => Array.from(arguments).reduce((summa, elem)=>summa+elem);
+
+const result2 = sumNumbers2(1,2,3);
+console.log(result2);
+
